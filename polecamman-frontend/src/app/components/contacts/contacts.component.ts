7af43dc7f3f21eda03f60ed805e31fb6413546c7
Bence Boér <bence0822@gmail.com>
2023-01-18 01:45:45 +0100

    contact info, wacky hamburger trigger

--- polecamman-frontend/src/app/components/contacts/contacts.component.ts
+++ polecamman-frontend/src/app/components/contacts/contacts.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {Contacts} from "../../data-types/Contacts";
 import {ContactService} from "../../services/contact.service";
 
@@ -10,11 +10,31 @@ import {ContactService} from "../../services/contact.service";
 export class ContactsComponent implements OnInit {
   @Input() open = true;
   @Input() contacts!: Contacts;
-  constructor(private contactService: ContactService) {}
+  shownInfo : any;
+
+  constructor(private contactService: ContactService) {
+  }
 
   ngOnInit(): void {
     this.contactService.getContacts().subscribe((contacts) => {
       this.contacts = contacts;
     });
+  }
+
+  showInfo(info: string) {
+    this.shownInfo = info;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hideInfo();
+  }
+
+  hideInfo() {
+    this.shownInfo = null;
+  }
+
+  copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text);
+    this.showInfo('Copied to clipboard');
   }
 }
