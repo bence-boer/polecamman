c9e6c140b1580bcce2ed878a54a1db5eaeb46085
Bence Boér <bence0822@gmail.com>
2023-01-26 05:27:23 +0100

    small scss fixes, delete spec.ts files

--- polecamman-frontend/src/app/components/contacts/contacts.component.ts
+++ polecamman-frontend/src/app/components/contacts/contacts.component.ts
@@ -1,25 +1,35 @@
-import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild} from '@angular/core';
 import {Contacts} from "../../data-types/Contacts";
 import {ContactService} from "../../services/contact.service";
+import {LanguageService} from "../../services/language.service";
 
 @Component({
   selector: 'contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit, AfterViewInit {
   @Input() open = true;
   contacts!: Contacts;
   shownInfo: any;
   displayInfo = false;
   infoOpacity = 0;
 
-  constructor(private contactService: ContactService) {
+  @ViewChild('aligner') aligner!: ElementRef;
+
+  constructor(private contactService: ContactService, private languageService: LanguageService) {
   }
 
   ngOnInit(): void {
     this.contactService.getContacts().subscribe((contacts) => {
       this.contacts = contacts;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.languageService.currentLanguage.subscribe((language) => {
+      let content = language === 'hu' ? 'Kapcsolat' : 'Contacts';
+      this.aligner.nativeElement.style.setProperty('--aligner-content', '"' + content + '"');
     });
   }
 
