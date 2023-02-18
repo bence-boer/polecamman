12f3d9d4ba4b3d60835a965e4df162b826f4ed19
Bence Boér <bence0822@gmail.com>
2023-02-18 02:36:05 +0100

    optimize preview image

--- polecamman-frontend/src/app/components/blog-post-preview/blog-post-preview.component.ts
+++ polecamman-frontend/src/app/components/blog-post-preview/blog-post-preview.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {BlogPost} from "../../data-types/BlogPost";
 import {environment} from "../../../environments/environment";
-import {MediaElement} from "../../data-types/MediaElement";
+import {MediaElement, MediaFormat} from "../../data-types/MediaElement";
 
 @Component({
   selector: 'blog-post-preview',
@@ -10,9 +10,10 @@ import {MediaElement} from "../../data-types/MediaElement";
 })
 export class BlogPostPreviewComponent{
   @Input() blogPost !: BlogPost | null;
-  thumbnail?: MediaElement;
+  media_0?: MediaElement;
   @Input() even !: boolean;
   serverURL = environment.serverURL;
+  thumbnail?: MediaFormat;
 
   multipleMedia = false;
   containsImage = false;
@@ -21,7 +22,8 @@ export class BlogPostPreviewComponent{
   ngOnChanges(){
     if(!this.blogPost) return;
     if (!this.blogPost.attributes.media.data.length) return;
-    this.thumbnail = this.blogPost.attributes.media.data[0].attributes;
+    this.media_0 = this.blogPost.attributes.media.data[0].attributes;
+    this.thumbnail = this.media_0.formats.large;
     this.multipleMedia = this.blogPost.attributes.media.data.length > 1;
     this.containsImage = this.blogPost.attributes.media.data.some(media => media.attributes.mime.includes('image'));
     this.containsVideo = this.blogPost.attributes.media.data.some(media => media.attributes.mime.includes('video'));
